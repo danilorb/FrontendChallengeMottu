12aa249fb5003ed0c5038a06daae8932353df100
Danilo Reis <daniloreis@MacBook-Pro-de-Danilo.local>
2024-10-01 10:15:08 -0300

    feat: Adiciono estrutura inicial do projeto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { FavoritesComponent } from './features/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent
+  }
+];
