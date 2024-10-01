85f6768061325e8d47ab93346880a45b70ec198d
Danilo Reis <daniloreis@MacBook-Pro-de-Danilo.local>
2024-10-01 14:38:10 -0300

    feat: Adiciono Material Angular ao projeto.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatFormFieldModule, MatInputModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
 
 }
