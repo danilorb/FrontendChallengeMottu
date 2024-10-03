1aa4dc15d9e6d0f17cad1e7b1aacfac469bf7736
Danilo Reis <daniloreis@MacBook-Pro-de-Danilo.local>
2024-10-03 02:51:05 -0300

    feat: Atualizo arquivo environments

--- src/app/core/services/character.service.ts
+++ src/app/core/services/character.service.ts
@@ -2,20 +2,20 @@ import { CharacterInterface } from './../interceptors/character.interface';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  private apiUrl = 'https://rickandmortyapi.com/api/character';
 
   constructor(private http: HttpClient) {}
 
   getCharacters(page: number): Observable<{ results: CharacterInterface[] }> {
-    return this.http.get<{ results: CharacterInterface[] }>(`${this.apiUrl}/?page=${page}`);
+    return this.http.get<{ results: CharacterInterface[] }>(`${environment.linkApi}/character?page=${page}`);
   }
 
   searchCharacters(name: string): Observable<{ results: CharacterInterface[] }> {
-    return this.http.get<{ results: CharacterInterface[] }>(`${this.apiUrl}/?name=${name}`);
+    return this.http.get<{ results: CharacterInterface[] }>(`${environment.linkApi}/character?name=${name}`);
   }
 }
